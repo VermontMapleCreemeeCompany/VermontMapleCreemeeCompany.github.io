24e98c5fcf69cabce5772f20af23ff2d39fe07a3
Levi Pare <levipare@gmail.com>
2025-02-21 10:58:26 -0500

    frontend style and cleanup

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,24 @@
+document.documentElement.style.setProperty('--height', window.innerHeight);
+
 window.addEventListener('scroll', () => {
   document.documentElement.style.setProperty('--scroll', document.documentElement.scrollTop);
 }, false);
 
+
+document.querySelector('.nav-toggle').addEventListener('click', () => {
+  document.querySelector('.bar nav').classList.toggle("active");
+})
+
+document.querySelectorAll('nav a').forEach((b) => {
+  b.addEventListener('click', () => {
+    document.querySelector('.bar nav').classList.remove("active");
+  })
+})
+
+
 document.querySelectorAll('.carousel button').forEach((b) => {
   b.addEventListener('click', () => {
-    let ul = b.parentElement.querySelector('ul');
+    let ul = b.parentElement.parentElement.querySelector('ul');
     if (b.classList.contains('next')) {
       ul.scrollBy({left: ul.offsetWidth, behavior: 'smooth'});
     } else {
