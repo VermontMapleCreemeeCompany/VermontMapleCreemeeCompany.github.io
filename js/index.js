5e8723016937db3a07f86c3cc440ef895dd998ce
Levi Pare <levipare@gmail.com>
2026-09-21 20:59:11 -0400

    feat(mail): switched to web3forms for backendless emailing

--- js/index.js
+++ js/index.js
@@ -21,53 +21,77 @@ document.querySelectorAll('.carousel button').forEach((b) => {
   b.addEventListener('click', () => {
     let ul = b.parentElement.parentElement.querySelector('ul');
     if (b.classList.contains('next')) {
-      ul.scrollBy({left: ul.offsetWidth, behavior: 'smooth'});
+      ul.scrollBy({ left: ul.offsetWidth, behavior: 'smooth' });
     } else {
-      ul.scrollBy({left: -ul.offsetWidth, behavior: 'smooth'});
+      ul.scrollBy({ left: -ul.offsetWidth, behavior: 'smooth' });
     }
   })
 })
 
 document.querySelector('input[type=tel]').addEventListener('input', (e) => {
-	let value = e.target.value;
-	let phoneNumber = value.replace(/\D/g, '');
+  let value = e.target.value;
+  let phoneNumber = value.replace(/\D/g, '');
 
-	if (phoneNumber.length < 4) {
-		return (e.target.value = phoneNumber);
-	} else if (phoneNumber.length < 7) {
-		return (e.target.value = `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`);
-	} else {
-		return (e.target.value = `(${phoneNumber.slice(0,3)}) ${phoneNumber.slice(3, 6)}-${phoneNumber.slice(6, 10)}`);
-	}
+  if (phoneNumber.length < 4) {
+    return (e.target.value = phoneNumber);
+  } else if (phoneNumber.length < 7) {
+    return (e.target.value = `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`);
+  } else {
+    return (e.target.value = `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3, 6)}-${phoneNumber.slice(6, 10)}`);
+  }
 });
 
-document.querySelector("#catering form").addEventListener('submit', async (e)=> {
+document.querySelector("#catering form").addEventListener('submit', async (e) => {
   e.preventDefault();
 
   let submitButton = e.submitter;
   submitButton.disabled = true;
-  
+
   let formData = new FormData(e.target);
-  let jsonData = Object.fromEntries(formData.entries())
-  
-	let res = await fetch("api/mailer",
-	  {
-	    method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-			body: JSON.stringify(jsonData),
-		}
-	);
+  formData.append("access_key", "8dc155be-f764-4eb9-a560-2665e3143ed5");
 
-	if (res.status == 200) {
-	    e.target.reset() // reset form fields
-	    alert("Thanks for reaching out. We will get back to you via email as soon as possible.");
-  } else {
-      alert("Uh Oh! Something went wrong on our end. Please send your inquiry to our email. info@vtmaplecreemee.com");
+  // Format date/times
+  let date = document.querySelector("input[name=date]").valueAsDate.toLocaleDateString("en-US", {
+    timeZone: "UTC",
+    month: "long",
+    day: "numeric",
+    year: "numeric"
+  });
+  let start = document.querySelector("input[name=start]").valueAsDate.toLocaleTimeString("en-US", {
+    timeZone: "UTC",
+    hour: "numeric",
+    minute: "2-digit"
+  });
+  let end = document.querySelector("input[name=end]").valueAsDate.toLocaleTimeString("en-US", {
+    timeZone: "UTC",
+    hour: "numeric",
+    minute: "2-digit"
+  });
+
+  formData.set("date", date);
+  formData.set("start", start);
+  formData.set("end", end);
+
+  try {
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      alert("Thank you for your interest! We will get back to you via email as soon as possible.");
+      e.target.reset()
+    } else {
+      alert("Error: " + data.message);
+    }
+
+  } catch (error) {
+    alert("Uh Oh! Something went wrong on our end. Please send your inquiry to our email. info@vtmaplecreemee.com");
+  } finally {
+    submitButton.disabled = false;
   }
-
-  submitButton.disabled = false;
 })
 
 
